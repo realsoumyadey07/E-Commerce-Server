4c97b857e5b08bd5e289303daaed7d4c70cece37
Soumyadip Dey <soumyadipdey802@gmail.com>
2025-10-06 19:50:21 +0530

    order controller got changed a bit

--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -1,11 +1,14 @@
 import express from "express";
-import { isAuthenticated } from "../middlewares/auth.middleware";
-import { cancelOrder, createOrder, getMyOrders } from "../controller/order.controller";
+import { authorizeRoles, isAuthenticated } from "../middlewares/auth.middleware";
+import { cancelOrder, createOrder, getAllOrders, getMyOrders } from "../controller/order.controller";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/create-order", isAuthenticated, createOrder);
 orderRouter.post("/cancel-order", isAuthenticated, cancelOrder);
 orderRouter.get("/my-orders", isAuthenticated, getMyOrders);
+
+// for admin
+orderRouter.get("/all-orders", isAuthenticated, authorizeRoles("admin"), getAllOrders);
 
 export default orderRouter;
